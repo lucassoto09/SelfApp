8db645c6a4b15efa1944d81dc481b6b002926191
lucassoto09 <66963156+lucassoto09@users.noreply.github.com>
2022-07-29 13:04:52 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -14,18 +14,25 @@ recognition.onresult = function(event) {
     console.log(Content);
 
     document.getElementById("textbox").innerHTML = Content;
-    speak();
+    if(Content =="take my selfie"){
+        console.log("taking selfie --- ")
+        speak();
+    }
 }
 
 function speak(){
     var synth =  window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking your Selfie in 5 second"
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function(){
+        take_snapshot();
+    }, 5000);
 }
 
 Webcam.set({
@@ -35,3 +42,16 @@ Webcam.set({
     png_quality:90
 });
 camera = document.getElementById("camera")
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
